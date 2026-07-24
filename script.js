e2a41aea0b131bce7353d626e392c00b341d97d7
Hasan <hasan.myoffice2@gmail.com>
2026-07-24 10:57:29 +0600

    Testimonials Section Complete

---
+++ script.js
@@ -0,0 +1,14 @@
+// const swiper = new Swiper(".Mytestimonials", {
+//   slidesPerView: 1,
+//   spaceBetween: 20,
+//   loop: true,
+
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   pagination: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
